2ec671fb6b99fd21b1d092341503dca9a38f364f
Arthutepio <arthurodriguesr10@gmail.com>
2022-12-04 10:44:48 -0300

    requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,5 @@
+SELECT ar.nome_artista AS 'artista',
+	     al.nome_album AS 'album'
+FROM SpotifyClone.artista AS ar
+INNER JOIN SpotifyClone.album AS al ON ar.id_artista = al.id_artista
+WHERE ar.nome_artista = 'Elis Regina';
